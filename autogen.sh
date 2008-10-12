88cc9a26e356f0481acf02c4c39dc147e09ff8e4
Anders Kaseorg <andersk@mit.edu>
2008-10-12 16:00:55 -0400

    Turn autogen.sh into a wrapper around autoreconf.
    The old autogen.sh did not work on Linux because of an incorrect check
    for libtoolize.
    
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- autogen.sh
+++ autogen.sh
@@ -1,41 +1,7 @@
-#!/bin/bash
-#
-# Run autotools for udis86
-#
-
-function search_exe {
-  for path in `echo $PATH | sed -e 's/:/ /g'` 
-  do
-    if [ -e ${path}/$1 ]; then
-       return 0
-    fi
-  done
-  return 1
-}
-
-function run_auto_tool {
-  echo autogen: running ... $*
-  $* || {
-    echo "autogen: failed to run $1"
-    exit 1
-  }
-}
-
-run_auto_tool aclocal
-
-# run libtoolize
-if [ ! \( -e libtoolize \) ]; then
-  echo autogen: libtoolize not found, looking for gtoolize 
-  search_exe glibtoolize # mac os x
-  if [ ! \( $? -eq 0 \) ];
-    then echo autogen: glibtoolize not found.
-    exit 1
-  fi
-  run_auto_tool glibtoolize --force --copy
-else
-  run_auto_tool libtoolize --force --copy
+#!/bin/sh
+if ! type libtoolize >/dev/null 2>&1 && type glibtoolize >/dev/null 2>&1; then
+  echo "$0: libtoolize not found, trying glibtoolize"
+  export LIBTOOLIZE=glibtoolize
 fi
-
-run_auto_tool autoheader
-run_auto_tool automake --add-missing --copy --foreign
-run_auto_tool autoconf
+cd "`dirname "$0"`"
+autoreconf --force -v --install
